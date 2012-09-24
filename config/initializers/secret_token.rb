961cb1462fdea1434efde902b5d1f3067c83f88e
Scott Lieberman <scottklieberman@gmail.com>
2012-09-24 14:12:23 -0400

    init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Motlee::Application.config.secret_token = '91fdfdd957fe23e37cf1cf36b081e238a510b32cedeabf45ad68e75f52f3fa627d248b5b902cd6dc406f26fbd9137282d442ccfd94e243b8758f5f231962c33c'
