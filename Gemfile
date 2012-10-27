96d3250eef9cbaec2856c3fa6378138bfdb32b7a
Scott Lieberman <scottklieberman@gmail.com>
2012-10-26 18:56:53 -0700

    Added Paperclip and migrated tables

--- Gemfile
+++ Gemfile
@@ -16,8 +16,8 @@ gem 'omniauth-facebook'
 gem 'activeadmin'
 gem 'curb'
 gem 'json'
-
-#gem 'paperclip', '~> 3.0'
+gem 'paperclip', '~>2.6.0'
+gem 'aws-sdk', '~>1.3.4'
 
 
 # Use unicorn as the web server
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121025230330) do
+ActiveRecord::Schema.define(:version => 20121027015521) do
 
   create_table "active_admin_comments", :force => true do |t|
     t.string   "resource_id",   :null => false
@@ -110,10 +110,14 @@ ActiveRecord::Schema.define(:version => 20121025230330) do
     t.string   "source"
     t.integer  "height"
     t.integer  "width"
-    t.float    "lat"
-    t.float    "lon"
+    t.decimal  "lat",                :precision => 15, :scale => 10
+    t.decimal  "lon",                :precision => 15, :scale => 10
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
   end
 
   create_table "stories", :force => true do |t|
