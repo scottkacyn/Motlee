07c005e8cefc9a51d85123d02140faf85eb47aa8
Zack Martinsek <zackmartinsek@ubuntu.ubuntu-domain>
2012-12-17 12:35:46 -0600

    Added omniauth.rb file for testing, staging and production servers

---
+++ config/initializers/omniauth.rb
@@ -0,0 +1,7 @@
+if Rails.env.production?
+  OmniAuth.config.full_host = "http://www.motleeapp.com"
+elsif Rails.env.test?
+  OmniAuth.config.full_host = "http://www.motleeapp.com"
+elsif Rails.env.development?
+  OmniAuth.config.full_host = "http://localhost.motleeapp.com"
+end
