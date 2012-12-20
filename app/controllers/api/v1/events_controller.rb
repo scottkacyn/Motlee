39147f38eb5ef7fa3190512f05e26d6d6ae91547
Scott Lieberman <scottklieberman@gmail.com>
2012-12-20 11:53:44 -0800

    Updating notifications event object

--- app/controllers/api/v1/events_controller.rb
+++ app/controllers/api/v1/events_controller.rb
@@ -63,7 +63,7 @@ class Api::V1::EventsController < ApplicationController
     # Requires :uids param which is an array of Facebook UIDs
     def join
         if (params[:type] == "invite")
-            @event = Event.where(:id => params[:event_id])
+            @event = Event.find(params[:event_id])
             @uids = params[:uids]
             @uid_array = []
             @uid_array = @uids.split(",")
@@ -84,10 +84,10 @@ class Api::V1::EventsController < ApplicationController
                 
                     # Now we check to see if the user has already been added to the event
                     @attendee = Attendee.where("user_id = ? AND event_id = ?", motlee_user.id, params[:event_id]).first
-                    Notifications.add_event_notification(motlee_user.id, @event, current_user)
                     if @attendee.nil?
                         # If user has not been added, create new Attendee object
                         @attendee = Attendee.create(:user_id => motlee_user.id, :event_id => params[:event_id], :rsvp_status => 1)
+                        Notifications.add_event_notification(motlee_user.id, @event, current_user)
                     end
                 end
             end
