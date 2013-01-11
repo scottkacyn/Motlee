9d62e819579f25ef6eee645c5197944da03066b9
Scott Lieberman <scottklieberman@gmail.com>
2013-01-11 09:25:41 -0800

    Slight tweak to the notifications.rb model and the way new friend joins are processed

--- app/jobs/add_friend_join_notification.rb
+++ app/jobs/add_friend_join_notification.rb
@@ -8,10 +8,9 @@ require 'resque'
 	joined_user = User.find(joined_user_id)
 	friends = joined_user.motlee_friends(access_token)
         current_date = DateTime.now
-        friends.each do |friend_id|
- 	    friend = User.find(friend_id)
+        friends.each do |friend|
             @notification_value = "Your friend #{joined_user.name} has just joined Motlee|friend|#{joined_user.id}|#{joined_user.id}|#{current_date}"
-            Notifications.add_notification(friend, @notification_value)	
+            Notifications.add_notification(friend.id, @notification_value)	
         end
     end
   end
