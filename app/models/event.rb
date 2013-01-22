0d52d7d8216ec0b5260c69f9dd7171d9749c0c6b
Scott Lieberman <scottklieberman@gmail.com>
2013-01-21 17:50:18 -0800

    Added has_one fb_og_attend to the evnets model

--- app/models/event.rb
+++ app/models/event.rb
@@ -5,6 +5,7 @@ class Event < ActiveRecord::Base
   has_many :photos
   has_many :stories
   has_one :user
+  has_one :fb_og_attend
   belongs_to :location
   has_many :attendees
   has_many :people_attending, :through => :attendees, :source => :user
