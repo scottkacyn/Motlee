4cef20524f9ff6fa1257a27746e0245bb495f2d2
Scott Lieberman <scottklieberman@gmail.com>
2013-02-05 10:58:45 -0800

    Changed all pass-thru values to be global @ variables

--- app/jobs/publish_facebook_attend.rb
+++ app/jobs/publish_facebook_attend.rb
@@ -4,28 +4,25 @@ module PublishFacebookAttend
 
     @queue = :facebook_attend
 
-    def self.perform(token, event_id, attendees)
-        @event_url = "http://staging.motleeapp.com/events/" + event_id
+    def self.perform(@token, @event_id, @attendees)
+        @event_url = "http://staging.motleeapp.com/events/" + @event_id
         # Scrape the two URLs so that the scraper info is up-to-date
         #
         Curl.post("https://graph.facebook.com?id=#{@event_url}&scrape=true");
-        @fbOgAttend = FbOgAttend.where(:event_id => event_id).first
+        @fbOgAttend = FbOgAttend.where(:event_id => @event_id).first
         if @fbOgAttend.nil?
-
             # Process the Facebook Open Graph action
-            #
             json = Curl.post("https://graph.facebook.com/me/motleeapp:attend",
-            {:access_token => token,
+            {:access_token => @token,
             :event => @event_url,
-            :tags => attendees})
-
+            :tags => @attendees})
             result = JSON.parse(json.body_str)
-            newFbAttend = FbOgAttend.create(:event_id => event_id, :fb_attend_id => result["id"])
+            newFbAttend = FbOgAttend.create(:event_id => @event_id, :fb_attend_id => result["id"])
         else
             Curl.post("https://graph.facebook.com/#{@fbOgAttend.fb_attend_id}",
-            {:access_token => token,
+            {:access_token => @token,
             :event => @event_url,
-            :tags => attendees})
+            :tags => @attendees})
         end
     end
 end
--- app/models/fb_og_attend.rb
+++ app/models/fb_og_attend.rb
@@ -1,6 +1,4 @@
 class FbOgAttend < ActiveRecord::Base
-
-  attr_accessible :fb_attend_id
+  attr_accessible :fb_attend_id, :event_id
   belongs_to :event
-
 end
