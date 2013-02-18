747c5b162f80227e6725e99bc9c4641406a76b18
Scott Lieberman <scottklieberman@gmail.com>
2013-02-18 13:21:17 -0800

    Added forking methods to prevent EOF errors on postgres

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -1,4 +1,11 @@
 Dir["#{Rails.root}/app/jobs/*.rb"].each { |file| require file }
 
-Resque.before_fork = Proc.new { 
-     ActiveRecord::ActiveRecord::Base.verify_active_connections! } 
+Resque.before_fork do
+    defined?(ActiveRecord::Base) and
+    ActiveRecord::Base.connection.disconnect!
+end
+
+Resque.after_fork do
+    defined?(ActiveRecord::Base) and
+    ActiveRecord::Base.establish_connection
+end
