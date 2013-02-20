6672dabb915082476ccbab5b09b89007bb0b06b5
Scott Lieberman <scottklieberman@gmail.com>
2013-02-20 13:57:58 -0800

    ...

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -1,3 +1,6 @@
 Dir["/app/jobs/*.rb"].each { |file| require file }
 
-Resque.before_fork = Proc.new { ActiveRecord::Base.establish_connection }
+Resque.after_fork do
+    defined?(ActiveRecord::Base) and
+    ActiveRecord::Base.establish_connection
+end
