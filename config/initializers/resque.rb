89952bae468590652e0cdb06a1165d824f66c318
Scott Lieberman <scottklieberman@gmail.com>
2013-02-20 13:52:58 -0800

    Changed resque.rb per Heroku support team recommendation

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -1,11 +1,3 @@
-Dir["/app/app/jobs/*.rb"].each { |file| require file }
+Dir["/app/jobs/*.rb"].each { |file| require file }
 
-Resque.before_fork do
-    defined?(ActiveRecord::Base) and
-    ActiveRecord::Base.connection.disconnect!
-end
-
-Resque.after_fork do
-    defined?(ActiveRecord::Base) and
-    ActiveRecord::Base.establish_connection
-end
+Resque.before_fork = Proc.new { ActiveRecord::Base.establish_connection }
