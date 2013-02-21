e5d6e59fbde4d31441633e00de1d619122cf7c4e
Scott Lieberman <scottklieberman@gmail.com>
2013-02-21 14:48:43 -0800

    Serve static assets to true in config files

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ require 'resque/server'
 Motlee::Application.routes.draw do
 
   mount Resque::Server, :at => "/resque"
+
   devise_for :users, :controllers => { :sessions => 'users/sessions', :omniauth_callbacks => 'users/omniauth_callbacks' }
 
   resources :users
