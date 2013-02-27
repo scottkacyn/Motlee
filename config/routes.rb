a53260a38b65cb796f4e3f80d8f1c717f9bc234b
Scott Lieberman <scottklieberman@gmail.com>
2013-02-26 21:16:11 -0800

    Reconfigured routes

--- config/routes.rb
+++ config/routes.rb
@@ -8,9 +8,8 @@ Motlee::Application.routes.draw do
   devise_for :users, :controllers => { :sessions => 'users/sessions', :omniauth_callbacks => 'users/omniauth_callbacks' }
 
   resources :users
-  resources :events, :only => [:show] do
-    resources :photos do
-    end
+  resources :events do
+    resources :photos
   end
   resources :locations
 
