3e6b63a2330f19319ae6f6b76e6e3959675de90f
Scott Lieberman <scottklieberman@gmail.com>
2013-03-14 10:49:39 -0700

    Oops

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -1,5 +1,5 @@
 Dir["/app/jobs/*.rb"].each { |file| require file }
 
 Resque.before_fork = Proc.new { 
-     ActiveRecord::ActiveRecord::Base.verify_active_connections!
+     ActiveRecord::Base.verify_active_connections!
 } 
