44e8ef8c3ecd10ecdd0e22635a2539c33dcef411
Scott Lieberman <scottklieberman@gmail.com>
2013-03-14 18:17:47 -0700

    added configatron init

---
+++ config/configatron/production.rb
@@ -0,0 +1,11 @@
+# Override your default settings for the Production environment here.
+# 
+# Example:
+#   configatron.file.storage = :s3
+
+# production (delivery):
+configatron.apn.host => 'gateway.push.apple.com'
+configatron.apn.cert => File.join(RAILS_ROOT, 'config', 'apple_push_notification_production.pem')
+
+configatron.apn.feedback.host => 'feedback.push.apple.com'
+configatron.apn.feedback.cert => File.join(RAILS_ROOT, 'config', 'apple_push_notification_production.pem')
---
+++ config/configatron/staging.rb
@@ -0,0 +1,9 @@
+configatron.apn.passphrase => 'dAl1nKx5L3NC'
+configatron.apn.port => 2195
+configatron.apn.host => 'gateway.sandbox.push.apple.com'
+configatron.apn.cert => File.join(RAILS_ROOT, 'config', 'apple_push_notification_development.pem')
+
+configatron.apn.feedback.passphrase => 'dAl1nKx5L3NC'
+configatron.apn.feedback.port => 2196
+configatron.apn.feedback.host => 'feedback.sandbox.push.apple.com'
+configatron.apn.feedback.cert => File.join(RAILS_ROOT, 'config', 'apple_push_notification_development.pem')
