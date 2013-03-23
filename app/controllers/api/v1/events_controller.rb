0b1fe172ced1b4bdf4da80e9529d8359d5911dcb
Scott Lieberman <scottklieberman@gmail.com>
2013-03-23 10:29:43 -0700

    Removed stories, why is that even still in there

--- app/controllers/api/v1/events_controller.rb
+++ app/controllers/api/v1/events_controller.rb
@@ -28,7 +28,6 @@ class Api::V1::EventsController < ApplicationController
         render :json => { :is_attending => is_attending,
                :event => @event.as_json({:methods => [:owner, :attendee_count], 
                :include => {:photos => {:include => {:comments => {:methods => [:owner]}, :likes => {:methods => [:owner]}}, :methods => :owner}, 
-               :stories => {}, 
                :people_attending => {:only => [:id, :uid, :name, :first_name, :last_name, :sign_in_count]}}})}
     end
 
