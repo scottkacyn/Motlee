4fc413ef722b3006d17d30d48bb46b96dd2eb434
Scott Lieberman <scottklieberman@gmail.com>
2013-04-02 12:33:49 -0700

    Whatever psuh you know dat

--- app/controllers/api/v1/events_controller.rb
+++ app/controllers/api/v1/events_controller.rb
@@ -27,7 +27,8 @@ class Api::V1::EventsController < ApplicationController
 
         render :json => { :is_attending => is_attending,
                :event => @event.as_json({:methods => [:owner, :attendee_count], 
-               :include => {:photos => {:include => {:comments => {:methods => [:owner]}, :likes => {:methods => [:owner]}}, :methods => :owner}}})}
+               :include => {:photos => {:include => {:comments => {:methods => [:owner]}, :likes => {:methods => [:owner]}}, :methods => :owner},
+               :people_attending => {:only => [:id, :name, :first_name, :last_name, :sign_in_count]}}})}
     end
 
     # POST
