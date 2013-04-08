ee908304940df53fdfcb6c1ca74aafdd9d3ae54c
Scott Lieberman <scottklieberman@gmail.com>
2013-04-08 12:30:31 -0700

    Rectifying the bullywasher

--- app/controllers/api/v1/photos_controller.rb
+++ app/controllers/api/v1/photos_controller.rb
@@ -55,7 +55,7 @@ class Api::V1::PhotosController < ApplicationController
 
         def update_caption
             @photo = current_user.photos.find(params[:id])
-            @photo.update_attributes(:caption => params[:caption], :updated_at => 'now()')
+            @photo.update_attributes(:caption => params[:caption])
 
             render :json => @photo.as_json
         end
