30ead43e641d448f476d81494167c29f1b213a6a
Scott Lieberman <scottklieberman@gmail.com>
2013-04-16 11:03:06 -0700

    Poop and scoop

--- app/jobs/add_comment_notification.rb
+++ app/jobs/add_comment_notification.rb
@@ -28,7 +28,7 @@ module AddCommentNotification
         comments.each do |userComment|
             if (owner_comment.id != userComment.user_id && owner_photo.id != userComment.user_id && @users_who_commented.include?(userComment.user_id))
                 @notification_value = "#{owner_comment.name} also commented on #{owner_photo.name}'s photo|photo_comment|#{commentable.id}|#{userComment.user_id}|#{current_date}"
-                Notifications.add_notification(commentable.user_id, @notification_value)
+                Notifications.add_notification(userComment.user_id, @notification_value)
                 PushNotification.add_comment_on_comment_notification(userComment.user_id, commentable, owner_comment)
 
                 # Remove the ID from array of IDs so we don't send multiple notifications
