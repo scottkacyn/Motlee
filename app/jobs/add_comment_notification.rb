1aa18a3c9080ee154025140c45a1d2e03bbdbd4c
Zack Martinsek <zackmartinsek@ubuntu.ubuntu-domain>
2013-04-16 09:27:02 -0500

    Added like/comment push notifications

--- app/jobs/add_comment_notification.rb
+++ app/jobs/add_comment_notification.rb
@@ -12,6 +12,22 @@ module AddCommentNotification
         if (commentable.user_id != owner_comment.id)	
             @notification_value = "#{owner_comment.name} commented on your photo|photo_comment|#{commentable.id}|#{owner_comment.id}|#{current_date}"
             Notifications.add_notification(commentable.user_id, @notification_value)
+			PushNotification.add_comment_notification(commentable.user_id, photo, owner_comment)
         end
+
+		comments = Comment.where(:commentable_id => commentable_id)
+
+		comments do |userComment|
+
+			owner = User.find(userComment.user_id)
+
+			if (owner_comment.id != owner.id && commentable.user_id != owner.id && userComment.id != comment.id)
+
+				@notification_value = "#{owner_comment.name} commented on a photo you commented on|photo_comment|#{commentable.id}|#{owner.id}|#{current_date}"
+            	Notifications.add_notification(commentable.user_id, @notification_value)
+				PushNotification.add_comment_notification(owner.id, photo, owner_comment)
+			end
+
+		end
     end
 end
--- app/models/push_notification.rb
+++ app/models/push_notification.rb
@@ -7,7 +7,43 @@ class PushNotification
             if (device.device_type == "Apple")
                 PushNotification.send_to_APNS(device.device_id, notification_value)
             elsif (device.device_type == "Android")
-                PushNotification.send_to_GCM(device.device_id, notification_value, inviter_user.id, event.id)
+                PushNotification.send_invite_to_GCM(device.device_id, notification_value, inviter_user.id, event.id)
+            end
+        end
+    end
+
+    def self.add_comment_notification(comment_user_id, photo, commenter_user)
+        devices = Device.where(:user_id => comment_user_id)
+        notification_value = "#{commenter_user.name} commented on your photo"
+        devices.each do |device|
+            if (device.device_type == "Apple")
+                PushNotification.send_to_APNS(device.device_id, notification_value)
+            elsif (device.device_type == "Android")
+                PushNotification.send_comment_like_to_GCM(device.device_id, notification_value, commenter_user.id, photo.id)
+            end
+        end
+    end
+
+    def self.add_like_notification(like_user_id, photo, liker_user)
+        devices = Device.where(:user_id => like_user_id)
+        notification_value = "#{commenter_user.name} liked your photo"
+        devices.each do |device|
+            if (device.device_type == "Apple")
+                PushNotification.send_to_APNS(device.device_id, notification_value)
+            elsif (device.device_type == "Android")
+                PushNotification.send_comment_like_to_GCM(device.device_id, notification_value, liker_user.id, photo.id)
+            end
+        end
+    end
+
+    def self.add_comment_on_comment_notification(comment_user_id, photo, commenter_user)
+        devices = Device.where(:user_id => comment_user_id)
+        notification_value = "#{commenter_user.name} commented on a photo you commented on"
+        devices.each do |device|
+            if (device.device_type == "Apple")
+                PushNotification.send_to_APNS(device.device_id, notification_value)
+            elsif (device.device_type == "Android")
+                PushNotification.send_comment_like_to_GCM(device.device_id, notification_value, commenter_user.id, photo.id)
             end
         end
     end
@@ -21,13 +57,25 @@ class PushNotification
         Urbanairship.push(notification)
     end
 
-    def self.send_to_GCM(device_id, message, inviter_id, event_id)
+    def self.send_invite_to_GCM(device_id, message, inviter_id, event_id)
         device = Gcm::Device.where(:registration_id => "#{device_id}").first_or_create
         notification = Gcm::Notification.new
         notification.device = device
         notification.collapse_key = "new_events"
         notification.delay_while_idle = true
         notification.data = {:registration_ids => ["#{device_id}"], :data => {:message_text => message, :inviter => inviter_id, :event_id => event_id}}
+        notification.save
+
+        response = Gcm::Notification.send_notifications
+    end
+
+    def self.send_comment_like_to_GCM(device_id, message, commenter_id, photo_id)
+        device = Gcm::Device.where(:registration_id => "#{device_id}").first_or_create
+        notification = Gcm::Notification.new
+        notification.device = device
+        notification.collapse_key = "new_comments_likes"
+        notification.delay_while_idle = true
+        notification.data = {:registration_ids => ["#{device_id}"], :data => {:message_text => message, :user_id => commenter_id, :photo_id => event_id}}
         notification.save
 
         response = Gcm::Notification.send_notifications
