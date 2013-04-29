d83667f2938e569ec8863919f923b54ed2a34883
Scott Lieberman <scottklieberman@gmail.com>
2013-04-29 11:53:49 -0700

    Minor tweaks to set_tags.rb

--- app/jobs/set_tags.rb
+++ app/jobs/set_tags.rb
@@ -5,32 +5,21 @@ module SetTags
     @queue = :set_tags
 
     def self.perform(event_id, photo_id, caption)
+        hashtags = caption.scan(/#\w+/)
+        final_tag_list = []
 
-		hashtags = caption.scan(/#\w+/)
+        hashtags.each do |hashtag|
+            hashtag.slice!(0)
+            final_tag_list << hashtag
+        end
+        
+        if final_tag_list.length > 0
+            event = Event.find(event_id)
+            photo = Photo.find(photo_id)
+            user = User.find(photo.user_id)
 
-		final_tag_list = []
-
-		hashtags.each do |hashtag|
-
-			hashtag.slice!(0)
-
-			final_tag_list << hashtag
-
-		end
-		
-		if final_tag_list.length > 0
-
-			event = Event.find(event_id)
-			photo = Photo.find(photo_id)
-			user = User.find(photo.user_id)
-
-			user.tag(photo, :with => final_tag_list)
-			event.tag_list.add(final_tag_list)			
-
-			# decided not to have user tag event for redundancy
-			# user.tag(event, :with => final_tag_list)
-
-		end
-
+            user.tag(photo, :with => final_tag_list)
+            event.tag_list.add(final_tag_list)			
+        end
     end
 end
