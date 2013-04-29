b9890b41bf4e05748a343e44890a7df562fdc99e
Scott Lieberman <scottklieberman@gmail.com>
2013-04-28 17:33:34 -0700

    Switched user_id to id in a users_controller params setting

--- app/controllers/api/v2/users_controller.rb
+++ app/controllers/api/v2/users_controller.rb
@@ -45,10 +45,10 @@ class Api::V2::UsersController < ApplicationController
 
   def notifications
     if (params[:type].nil? or params[:type] == "unread")
-        key = "#{params[:user_id]}:unread"
+        key = "#{params[:id]}:unread"
     	render :json => REDIS.lrange(key, 0, -1)
     elsif (params[:type] == "all")
-	render :json => Notifications.get_notifications_mark_read(params[:user_id])	
+	render :json => Notifications.get_notifications_mark_read(params[:id])	
     end
   end
 
